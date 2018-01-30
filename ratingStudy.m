3c62f949f26e74e040f0866d3cd66fcaa1633408
JAQuent <alexander.quent@rub.de>
2018-01-30 11:05:15 +0000

    Version 1.0

--- ratingStudy.m
+++ ratingStudy.m
@@ -1,8 +1,8 @@
 function [] = ratingStudy(subNo)
 % % % % % % % % % % % % % % % % % % % % % % % % % 
-% Rating study
+% Rating study (Post encoding)
 % Author: Alexander Quent (alex.quent at mrc-cbu.cam.ac.uk)
-% Version: 1.1
+% Version: 1.0
 % % % % % % % % % % % % % % % % % % % % % % % % %
 
 %% Explanations
@@ -22,48 +22,28 @@ try
     % General information about subject and session
     date  = str2double(datestr(now,'yyyymmdd'));
     time  = str2double(datestr(now,'HHMMSS'));
-    subNo = num2str(subNo);
     
     % Input variables
-    [ratingType, fileNames, questions] = textread(strcat('stimuli/questions', subNo, '.txt'),'%10s %20s %100s', 'delimiter','\t');
-    [currentTrial, finished] = textread(strcat('log', subNo, '.txt'),'%n %n', 'delimiter','\t');
-    if finished == 1
-        error('You are already finished with the task. Please send me results.')
-    else
-        logPointer   = fopen(strcat('log', subNo, '.txt'), 'w');
-        fprintf(logPointer,'\r%4d %4d', 1, 0);
-        mSave        = strcat('data/ratingStudy_', subNo,'.mat'); % name of another data file to write to (in .mat format)
-        mSaveALL     = strcat('data/ratingStudy_', subNo,'all.mat'); % name of another data file to write to (in .mat format)
-    end
-    nTrial      = length(fileNames);
-    nPracTrial  = 8;
-    numObject   = nTrial/21;
-    numLocation = 20;
-    matrixDim   = [numObject, numLocation];
-    index1      = [];
-    index2      = [];
-    % This bit parses whether the question (object vs. location) needs one
-    % or two indices for the respective matrix.
+    [fileNames, questions] = textread('stimuli/questions.txt', '%20s %100s', 'delimiter', '\t');
+    nTrial                 = length(fileNames);
+    shuffle                = randomOrder(nTrial, nTrial);
+    fileNames              = fileNames(shuffle);
+    questions              = questions(shuffle);
+    
+    % This bit parses objects and locations
+    objects   = [];
+    locations = [];
     for i = 1:nTrial
-        if strcmp(ratingType{i}, 'object')
-            index1(i) = str2double(fileNames{i});
-            index2(i) = 1;
-        else
-            splitStr = strsplit(fileNames{i}, '_');
-            index1(i) = str2double(splitStr{1}); % Object
-            index2(i) = str2double(splitStr{2}); % Location
-        end
+        splitStr     = strsplit(fileNames{i}, '_');
+        objects(i)   = str2double(splitStr{1}); % Object
+        locations(i) = str2double(splitStr{2}); % Location
     end
     
-    % Output variables
-    if currentTrial > 1
-        load(strcat('data/ratingStudy_', num2str(subNo), '.mat'))
-    else
-        objectRatings   = zeros(numObject, 1) -999;
-        locationRatings = zeros(matrixDim(1), matrixDim(2)) - 999;
-        objectRT        = zeros(numObject, 1) - 999;
-        locationRT      = zeros(matrixDim(1), matrixDim(2)) - 999;
-    end
+    % Output variables & files
+    filePointer = fopen(strcat('data/ratingStudy_' , num2str(subNo),'.dat'),'wt'); % opens ASCII file for writing
+    mSave       = strcat('data/ratingStudy_', num2str(subNo),'.mat'); % name of another data file to write to (in .mat format)
+    mSaveALL    = strcat('data/ratingStudy_', num2str(subNo),'all.mat'); % name of another data file to write to (in .mat format)
+    
 
     % Get information about the screen and set general things
     Screen('Preference', 'SuppressAllWarnings',0);
@@ -75,9 +55,6 @@ try
         screenNum        = 0;
     end
     rect             = Screen('Rect', screenNum); % Gets the dimension of one screen
-    screenSize       = [0 0 1000 900];
-    screenPosition   = CenterRectOnPoint(screenSize,rect(3)/2, rect(4)/2); % Draws the window in the middle of one screen
-
     % Relevant key codes
     KbName('UnifyKeyNames');
     space  = KbName('space');
@@ -87,45 +64,36 @@ try
     bgColor       = [255 255 255];
     textSize      = [20 20];
     lineLength    = 60;
-    messageIntro1 = WrapString('Rating study \n\n First, your task is to rate the expectancy of specific objects at specific locations in a kitchen. A generally unexpected object may be more or less expected depending on the location. The scale ranges from unexpected (-100) to expected (100). Move your mouse to move the slider across the scale. You have to rate 400 objects/locations. After completing those, you will be asked again to rate the general expectancy of the objects in a kitchen. \n\n You are able to pause the experiment after each trial, so you don’t have to do that in one go. After completion, just send me your results. Thank you so much for your help. To calibriate your object/location ratings, you will start with eight practice trials with representative objects and locations. Press spacebar to start the practice run.',lineLength);
-    messageIntro2 = WrapString('End of practice \n\n Press spacebar to start the experiment.',lineLength);
-    messageIntro3 = WrapString('Object ratings \n\n Press spacebar to continue.',lineLength);
+    ITI           = 0.5;
+    messageIntro1 = WrapString('Rating study \n\n Your task is to rate the expectancy of specific objects at specific locations in a kitchen. A generally unexpected object may be more or less expected depending on the location. The scale ranges from unexpected (-100) to expected (100). Move your mouse to move the slider across the scale. You have seen all objects in the previous part of the experiment. \n\n Press spacebar to start.',lineLength);
     endPoints     = {'unexpected', 'expected'};
 
     % Opening window and setting preferences
     try
-        [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor, screenPosition);
+        [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor);
     catch
         try
-            [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor, screenPosition);
+            [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor);
         catch
             try
-                [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor, screenPosition);
+                [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor);
             catch
                 try
-                    [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor, screenPosition);
+                    [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor);
                 catch
-                    [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor, screenPosition);
+                    [myScreen, rect]    = Screen('OpenWindow', screenNum, bgColor);
                 end
             end
         end
     end
 
     % Loading stimuli
-    pracQuestions = {'How expected are these peppers in that location?', 'How expected is that wrench in that location?', 'How expected in this dumbbell in that location?', 'How expected are these peppers in that location?', 'How expected in this dumbbell in that location?', 'How expected is this pot in that location?', 'How expected is this pot in that location?', 'How expected is this wrench in that location?'};
-    for i = 1:nPracTrial
-        pracImages{i} = imresize(imread(strcat('stimuli/prac', num2str(i), '.png')),0.6987);
-    end
     for i = 1:length(fileNames)
-        if length(fileNames{i}) < 7
-            images{i}  = imresize(imread(strcat('stimuli/', fileNames{i}, '.png')),0.6987);
-        else
-            images{i}  = imresize(imread(strcat('stimuli/', fileNames{i}, '.png')), 0.5);
-        end
+        images{i}  = imresize(imread(strcat('stimuli/', fileNames{i}, '.png')), 0.5);
     end
 
 %% Experimental loop
-    for trial = currentTrial:nTrial
+    for trial = 1:nTrial
         % Exercise and instruction
         if trial == 1
             % Page 1
@@ -139,102 +107,47 @@ try
             end
 
             Screen('TextSize', myScreen, textSize(2)); % Sets size to normal
-            
-            %Practice
-            for pracTrial = 1:nPracTrial
-                slideScale(myScreen, pracQuestions{pracTrial}, rect, endPoints, 'device', 'mouse', 'image', pracImages{pracTrial},'scalaposition', 0.9, 'startposition', 'center', 'displayposition', true, 'aborttime', 200);
-            
-                % Escape or continue
-                DrawFormattedText(myScreen, strcat('Press space to continue or esacpe to pause experiment. \n Progress: ', num2str(round(trial/nTrial, 2)*100), ' %'), 'center', 'center');
-                Screen('Flip', myScreen);
-                KbReleaseWait;
-                [~, ~, keyCode] = KbCheck; 
-                while keyCode(escape) == 0 && keyCode(space) == 0
-                    [~, ~, keyCode] = KbCheck;
-                end
-            end
-            
-            clearvars pracImages % deletes extraneous variable
-            
-            % Page 2
-            Screen('TextSize', myScreen, textSize(1)); % Sets size to instruction size
-            DrawFormattedText(myScreen, messageIntro2, 'center', 'center');
-            Screen('Flip', myScreen);
-            KbReleaseWait;
-            [~, ~, keyCode] = KbCheck; 
-            while keyCode(space) == 0 
-                [~, ~, keyCode] = KbCheck;
-            end
-
-            Screen('TextSize', myScreen, textSize(2)); % Sets size to normal
-        elseif trial == 401
-            % Page 2
-            Screen('TextSize', myScreen, textSize(1)); % Sets size to instruction size
-            DrawFormattedText(myScreen, messageIntro3, 'center', 'center');
-            Screen('Flip', myScreen);
-            KbReleaseWait;
-            [~, ~, keyCode] = KbCheck; 
-            while keyCode(space) == 0 
-                [~, ~, keyCode] = KbCheck;
-            end
-
-            Screen('TextSize', myScreen, textSize(2)); % Sets size to normal
         end 
         %% Trial
         [position, RT] = slideScale(myScreen, questions{trial}, rect, endPoints, 'device', 'mouse', 'image', images{trial},'scalaposition', 0.9, 'startposition', 'center', 'displayposition', true, 'aborttime', 200);
-        if strcmp(ratingType{trial}, 'object')
-            objectRatings(index1(trial), index2(trial))   = position;
-            objectRT(index1(trial), index2(trial))        = RT;
-        else
-            locationRatings(index1(trial), index2(trial)) = position;
-            locationRT(index1(trial), index2(trial))      = RT;
-        end
+
+        %% Saving data
+        fprintf(filePointer,'%i %i %i %i %i %i %f %f\n', ...
+            subNo,...
+            date,...
+            time,...
+            trial,...
+            objects(trial),...
+            locations(trial),...
+            position,...
+            RT);
         
-        % Update files
-        logPointer   = fopen(strcat('log', subNo, '.txt'), 'w');
-        fprintf(logPointer,'\r%4d %4d', trial, finished);
-        dlmwrite(strcat('data/objectRatings_' , num2str(subNo),'.dat'), objectRatings, 'delimiter', '\t', 'precision', 6);
-        dlmwrite(strcat('data/locationRatings_' , num2str(subNo),'.dat'), locationRatings, 'delimiter', '\t', 'precision', 6);
-        dlmwrite(strcat('data/objectRT_' , num2str(subNo),'.dat'), objectRT, 'delimiter', '\t', 'precision', 6);
-        dlmwrite(strcat('data/locationRT_' , num2str(subNo),'.dat'), locationRT, 'delimiter', '\t', 'precision', 6);
+        results{trial, 1} = subNo;
+        results{trial, 2} = date;
+        results{trial, 3} = time;
+        results{trial, 4} = trial;
+        results{trial, 5} = objects(trial);
+        results{trial, 6} = locations(trial);
+        results{trial, 7} = position;
+        results{trial, 8} = RT;
         
-        % Escape or continue
-        DrawFormattedText(myScreen, strcat('Press space to continue or esacpe to pause experiment. \n Progress: ', num2str(round(trial/nTrial, 2)*100), ' %'), 'center', 'center');
+        %% ITI
         Screen('Flip', myScreen);
-        KbReleaseWait;
-        [~, ~, keyCode] = KbCheck; 
-        while keyCode(escape) == 0 && keyCode(space) == 0
-            [~, ~, keyCode] = KbCheck;
-        end
-        if keyCode(escape) == 1
-            break
-        end
+        WaitSecs(ITI);
     end          
        
     %% End of experiment
-    if trial == nTrial
-        finished = 1;
-        Screen('TextSize', myScreen, textSize(1)); % Sets size to instruction size
-        DrawFormattedText(myScreen, horzcat('End of experiment. Thank you for your participation. \n Please press escape to leave.'), 'center', 'center');
-        Screen('Flip', myScreen); 
-        [~, ~, keyCode] = KbCheck; 
-        while keyCode(escape) == 0 
-            [~, ~, keyCode] = KbCheck;
-        end
-    else
-        Screen('TextSize', myScreen, textSize(1)); % Sets size to instruction size
-        DrawFormattedText(myScreen, horzcat('The experiment has been paused. You can continue later. \n Please press escape to leave.'), 'center', 'center');
-        Screen('Flip', myScreen); 
-        [~, ~, keyCode] = KbCheck; 
-        while keyCode(escape) == 0 
-            [~, ~, keyCode] = KbCheck;
-        end
+    Screen('TextSize', myScreen, textSize(1)); % Sets size to instruction size
+    DrawFormattedText(myScreen, horzcat('End of experiment. Thank you for your participation. \n Please press escape to leave.'), 'center', 'center');
+    Screen('Flip', myScreen); 
+    [~, ~, keyCode] = KbCheck; 
+    while keyCode(escape) == 0 
+        [~, ~, keyCode] = KbCheck;
     end
+
     clearvars images
-    logPointer   = fopen(strcat('log', subNo, '.txt'), 'w');
-    fprintf(logPointer,'\r%4d %4d', trial, finished);
     fclose('all');
-    save(mSave, 'objectRatings', 'objectRT','locationRatings','locationRT');
+    save(mSave, 'results');
     save(mSaveALL);
     Screen('CloseAll')
 catch
